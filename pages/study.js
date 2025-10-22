940f82d403e192f214ae1f8bc56775f0167136b0
AVO <avanomme@upei.ca>
2025-10-22 14:55:42 -0300

    Add StudyPage component to render flashcards

---
+++ pages/study.js
@@ -0,0 +1,17 @@
+```javascript
+import React from "react";
+import FlashcardApp from "../components/FlashcardApp";
+
+const StudyPage = () => {
+  return (
+    <div>
+      <h1>Study</h1>
+      <p>Study your flashcards below.</p>
+      <FlashcardApp />
+    </div>
+  );
+};
+
+export default StudyPage;
+
+```
